348a502d3f30e48dd42fcdc9fa9e444550537093
Mohamed Firas <62911996+Fir121@users.noreply.github.com>
2024-10-13 21:08:15 +0400

    mysql

--- myproject/settings.py
+++ myproject/settings.py
@@ -108,8 +108,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'kwUSRYqxdJpUAwAKkVnKMDPWxMbLIDtY',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '30638',
     }
 }
 
